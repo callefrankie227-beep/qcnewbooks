2fd0de23e2854b46ab932c605a45eec212ecfe2e
Frankie Calle <frankie.calle.227@gmail.com>
2026-04-27 20:26:02 -0400

    finished 2.3 - html nesting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
     <span>Net Revenue</span>
     <span>$145,000</span>
   </div>  <div>
-    <span>Net Income</span>
+    <span><strong>Net Income</strong></span>
     <span>$85,000</span>
   </div>
+
